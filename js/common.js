4a9347e9e644f6df16a99cd383f1857dc6bf4e61
jingjiajie <932166095@qq.com>
2018-08-06 22:23:19 +0800

    finished stock search

--- js/common.js
+++ js/common.js
@@ -6,3 +6,21 @@ function getQueryParam(name) {
     var r = window.location.search.substr(1).match(reg);//search,查询？后面的参数，并匹配正则
     if (r != null) return unescape(r[2]); return null;
 }
+
+Date.prototype.format = function (fmt) { //author: meizz   
+    var o = {
+        "M+": this.getMonth() + 1,                 //月份   
+        "d+": this.getDate(),                    //日   
+        "h+": this.getHours(),                   //小时   
+        "m+": this.getMinutes(),                 //分   
+        "s+": this.getSeconds(),                 //秒   
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度   
+        "S": this.getMilliseconds()             //毫秒   
+    };
+    if (/(y+)/.test(fmt))
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt))
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
